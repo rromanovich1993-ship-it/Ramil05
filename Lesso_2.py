dd7ee877b4e3feab386b7c76034f6c0de2e8f21d
rromanovich1993-ship-it <r.romanovich1993@gmail.com>
2026-05-12 00:44:17 +0400

    Прокачка первого урока окончена

---
+++ Lesso_2.py
@@ -0,0 +1,28 @@
+# # СТРОКИ
+# text = " Hello  World"
+# print(text)
+# # инденксы
+# text_1 = "Python"
+# # result = text_1[-1]
+# # print(result)
+# # срезы
+# result_1 = text_1[2:6:1]
+# print(result_1)
+# # Полидром
+# text_2 = "Python"
+# result_2 = text_2[::-1]
+# print(result_2)
+# Срезы
+# price = "777 топоры"
+# result = price[:3]
+# print(result)
+# Конкотанация
+# item_price = "BMW"
+# item_zena = "600$"
+# result = f" Я купил {item_price} за {item_zena}"
+# print(result)
+# text_3 = "прокачка питона"
+# result_3 = text_3.replace("прокачка", "осиление")
+text_4 = "Python QAQ rules"
+assert  "QQ" in text_4, "HERNYA"
+
--- Lesson_1.py
+++ Lesson_1.py
@@ -1,1 +1,35 @@
 # Переменные и типы данных
+# # №1
+# city = "Saratov"
+# population = 850000
+# print(city)
+# print(population)
+# # №2
+# age_user_1 = 25
+# max_speed = 120
+# attempt_count = 3
+# print(max_speed)
+# print(attempt_count)
+# print(age_user_1)
+# # №3
+# int_lesson = 10
+# float_lesson = 3.14
+# str_lesson = "Hello"
+# boll_lesson = True
+# print(type(int_lesson))
+# print(type(float_lesson))
+# print(type(str_lesson))
+# print(type(boll_lesson))
+# # №4
+# a =  42
+# b = "42"
+# c = 42.0
+# print("переменная a - это", type(a))
+# print("переменная b - это", type(b))
+# print("переменная c - это", type(c))
+stroka_1 = "123"
+stroka_2 = 45.67
+print(int(stroka_1) + 10)
+print(int(stroka_2))
+print(str(stroka_2) + " рублей")
+
