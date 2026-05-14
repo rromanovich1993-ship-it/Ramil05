69c74d99c048ef559b88ec1f522d8f921f1a8b1e
rromanovich1993-ship-it <r.romanovich1993@gmail.com>
2026-05-14 19:19:46 +0400

    урок прохождение

---
+++ Lesson_3.py
@@ -0,0 +1,15 @@
+# Списки
+cars = ["MERS", "BMW", "AUDI","MERS"]
+# cars.append("Lada")
+# cars.insert(0,"NIVA")
+# cars[4] = "id"
+# cars.remove("MERS")
+# del cars[3]
+# cars.pop(3)
+# delete_element = cars.pop(0)
+# print(delete_element)
+# text = ["a", "b", "c"]
+# resul = text[0]
+# print(resul)
+# text_1 = [1, 2, 3, ["a", "b", "c"], 5 ]
+# print(text_1[3][2])
