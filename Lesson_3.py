2582ef25a8379f0a98eb7d7eff90f2da7bd0e7ae
rromanovich1993-ship-it <r.romanovich1993@gmail.com>
2026-05-14 19:19:46 +0400

    Домашка

---
+++ Lesson_3.py
@@ -0,0 +1,21 @@
+# Домашние задание
+# colors = ["красный", "синий", "зеленый", "желтый", "фиолетовый"]
+# print(colors[0])
+# print(colors[4])
+# fruits = ["яблоко", "банан", "вишня"]
+# fruits.append("апельсин")
+# fruits.insert(1,"киви")
+# print(fruits)
+# numbers = [10, 20, 30, 40, 50]
+# numbers.remove(30)
+# numbers.pop(2)
+# numbers.clear()
+# print(numbers)
+# letters = ["a", "b", "c", "d", "e", "f", "g"]
+# print(letters[2:5])
+# print(letters[:3])
+# list1 = [1, 2, 3]
+# list2 = [4, 5, 6]
+# result = list1.extend(list2)
+# print(list1)
+# print(list2)
