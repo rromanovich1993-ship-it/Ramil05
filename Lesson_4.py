219dafbbdc7a54bdb81fe282e35d5ec22eea2c7d
rromanovich1993-ship-it <r.romanovich1993@gmail.com>
2026-05-15 21:52:36 +0400

    Условные операторы ещё

--- Lesson_4.py
+++ Lesson_4.py
@@ -1,5 +1,8 @@
 # Условные операторы
-age = int(input("Ведите число "))
-dol_1 = str(input("Ведите профессию"))
-if age >= 18 or dol_1 == "студент":
-    print("Ты крут")
+# age = int(input("Ведите число "))
+# dol_1 = str(input("Ведите профессию"))
+# if age >= 18 or dol_1 == "студент":
+#     print("Ты крут")
+number = int(input("Enter a number: "))
+result = "четное" if number % 2 == 0 else "нечетное"
+print(result)
