29c0735e2c36525106b44a403bf9541426c8e33a
rromanovich1993-ship-it <r.romanovich1993@gmail.com>
2026-05-17 02:06:36 +0400

    циклы

--- Lesson_4.py
+++ Lesson_4.py
@@ -1,12 +1,27 @@
-# Словари
-import json
-homan = {
-    "name": "Alex",
-    "age": 41,
-    "dota": {
-        "color": "raq"
-    }
-}
-
-print(json.dumps(homan ,indent=4))
-
+# Циклы
+# frutic = ["яблоко", "груша", "виноград"]
+# for frut_1 in frutic:
+#     print(frut_1)
+# message = "Hello World"
+# for letter in message:
+#     print(letter.upper())
+# homan = {
+#     "name": "ALEX",
+#     "age": 42,
+#     "city": "Stavropol"
+# }
+# for key, value in homan.items():
+#     print(f"{key}: {value}")
+# print(list(range(1,10)))
+# count = 0
+# while count < 10:
+#     print(f"Счетчик = {count}")
+#     count += 1
+while True:
+    answer = input("Хотите продолжить? да/нет")
+    if answer == "нет":
+        break
+    elif answer == "да":
+        continue
+    else:
+        print("Читай внимательнее")
