bf8273e3cb87b850f5e60cce8193701bc03b8165
rromanovich1993-ship-it <r.romanovich1993@gmail.com>
2026-05-17 14:00:39 +0400

    файлы

--- Lesson_4.py
+++ Lesson_4.py
@@ -1,27 +1,6 @@
-# Циклы
-# frutic = ["яблоко", "груша", "виноград"]
-# for frut_1 in frutic:
-#     print(frut_1)
-# message = "Hello World"
-# for letter in message:
-#     print(letter.upper())
-# homan = {
-#     "name": "ALEX",
-#     "age": 42,
-#     "city": "Stavropol"
-# }
-# for key, value in homan.items():
-#     print(f"{key}: {value}")
-# print(list(range(1,10)))
-# count = 0
-# while count < 10:
-#     print(f"Счетчик = {count}")
-#     count += 1
-while True:
-    answer = input("Хотите продолжить? да/нет")
-    if answer == "нет":
-        break
-    elif answer == "да":
-        continue
-    else:
-        print("Читай внимательнее")
+# Файлы
+with open("message","r",encoding="utf-8") as file:
+    file_content=file.read()
+    print(file_content)
+
+file.write("fsdfdsda")
