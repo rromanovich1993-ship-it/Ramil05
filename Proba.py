e46c610c8caa8e81b7ff896938faf6507f938ae4
rromanovich1993-ship-it <r.romanovich1993@gmail.com>
2026-05-30 11:16:12 +0400

    Селениум ещё

--- Proba.py
+++ Proba.py
@@ -10,7 +10,17 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get("https://avia.tutu.ru/f/Sarato")
 time.sleep(2)
-print(driver.current_url)
-assert driver.current_url == "https://avia.tutu.ru/f/Saratov", "Открыта неверная страница"
+page_tittle = "https://avia.tutu.ru/f/Saratov"
+if driver.title == page_tittle:
+    print("Перешли на нужную страницу")
+else:
+    print(f"Ошибка текущий заголовок '{page_tittle}' не соответсвует ожиданию")
+# current_1 = driver.current_url
+# if current_1 == "https://avia.tutu.ru/f/Saratov":
+#     print("Нужна страница")
+# else:
+#     print("Ошибка")
+
+# assert driver.current_url == "https://avia.tutu.ru/f/Saratov", "Открыта неверная страница"
 
 
